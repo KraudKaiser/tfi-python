1de4edad50b452f2cdcfde97d2b0aa9eb9922d51
RamiroPc <ramirogimenezcontacto@gmail.com>
2025-07-02 19:58:10 -0300

    saved changes

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-from trimestres import ingreso_trimestre
+import trimestres
+import pedidos
+import estadisticas
+
 def main():
-    ingreso_trimestre()
+    anio, trimestre = trimestres.ingreso_trimestre()
+    lista_pedidos = pedidos.ingresar_pedidos(anio, trimestre)
+    estadisticas.mostrar_estadisticas(lista_pedidos)
+
+main()
